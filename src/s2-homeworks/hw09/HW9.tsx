9e8b183ddb6d06b22137fbe74b421d37a5d2df55
Anna-Bekreneva <anjabekreneva@gmail.com>
2023-01-01 15:58:34 +0300

    hw 9

--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import Clock from './Clock'
-import s2 from '../../s1-main/App.module.css'
+import React from 'react';
+import Clock from './Clock';
+import s2 from '../../s1-main/App.module.css';
 
 /*
-* 1 - в файле Clock.tsx дописать функции stop, start, onMouseEnter, onMouseLeave
-* 2 - в файле Clock.tsx из переменной date вычислить значения stringTime, stringDate, stringDay, stringMonth
-* 3 - в файле Clock.tsx дизэйблить кнопки стоп / старт если таймер не запущен / запущен соответственно
-* 4 - сделать стили в соответствии с дизайном
-* */
+ * 1 - в файле Clock.tsx дописать функции stop, start, onMouseEnter, onMouseLeave
+ * 2 - в файле Clock.tsx из переменной date вычислить значения stringTime, stringDate, stringDay, stringMonth
+ * 3 - в файле Clock.tsx дизэйблить кнопки стоп / старт если таймер не запущен / запущен соответственно
+ * 4 - сделать стили в соответствии с дизайном
+ * */
 
 const HW9 = () => {
-    return (
-        <div id={'hw9'}>
-            <div className={s2.hwTitle}>Homework #9</div>
+	return (
+		<section className={s2.section} id={'hw9'}>
+            <div className={s2.container}>
+                <h2 className={s2.hwTitle}>Homework #9</h2>
+             </div>
 
-            <div className={s2.hw}>
-                <Clock />
-            </div>
-        </div>
-    )
-}
+            <div className={s2.wrapper}>
+            	<div className={`${s2.hw} ${s2.container}`}>
+                	<Clock/>
+           		 </div>
+			</div>
+        </section>
+	);
+};
 
-export default HW9
+export default HW9;
